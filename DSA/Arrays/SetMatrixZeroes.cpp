1dec6161ad7d2feda83cb3396d0e73dd41601fbe
Bhagyashri Hiremath <bhagyashrihiremath11@gmail.com>
2025-09-22 19:11:00 +0530

    Update SetMatrixZeroes.cpp

--- DSA/Arrays/SetMatrixZeroes.cpp
+++ DSA/Arrays/SetMatrixZeroes.cpp
@@ -1,54 +1,60 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
-public:
-    void setZeroes(vector<vector<int>>& matrix) {
-        int m = matrix.size();
-        int n = matrix[0].size();
-UU6uu        
-        vector<int> row(m, 0), col(n, 0);
-        
-        // First pass: mark rows and cols
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                if(matrix[i][j] == 0){
-                    row[i] = 1;
-                    col[j] = 1;
-                }
-            }
-        }
-        
-        // Second pass: set zeroes
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                if(row[i] == 1 || col[j] == 1){
-                    matrix[i][j] = 0;
-                }
+vector<vector<int>> zeroMatrix(vector<vector<int>> &matrix, int n, int m) {
+    int col0 = 1;
+
+    // Step 1: Mark first row and column
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (matrix[i][j] == 0) {
+                matrix[i][0] = 0;
+                if (j != 0)
+                    matrix[0][j] = 0;
+                else
+                    col0 = 0;
             }
         }
     }
-};
+
+    // Step 2: Use markers to set zeros
+    for (int i = 1; i < n; i++) {
+        for (int j = 1; j < m; j++) {
+            if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+
+    // Step 3: Handle first row
+    if (matrix[0][0] == 0) {
+        for (int j = 0; j < m; j++) {
+            matrix[0][j] = 0;
+        }
+    }
+
+    // Step 4: Handle first column
+    if (col0 == 0) {
+        for (int i = 0; i < n; i++) {
+            matrix[i][0] = 0;
+        }
+    }
+
+    return matrix;
+}
 
 int main() {
-    // Example input
-    vector<vector<int>> matrix = {
-        {1, 1, 1},
-        {1, 0, 1},
-     u   {1, 1, 1}
-    };
+    vector<vector<int>> matrix = {{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
+    int n = matrix.size();
+    int m = matrix[0].size();
+    vector<vector<int>> ans = zeroMatrix(matrix, n, m);
 
-    Solution sol;
-    sol.setZeroes(matrix);
-
-    // Output result
-    cout << "Matrix after setting zeroes:\n";
-    for(auto &row : matrix) {
-        for(auto &val : row) {
-            cout << val << " ";
+    cout << "The Final matrix is:\n";
+    for (auto &row : ans) {
+        for (auto &ele : row) {
+            cout << ele << " ";
         }
         cout << "\n";
     }
-
     return 0;
 }
